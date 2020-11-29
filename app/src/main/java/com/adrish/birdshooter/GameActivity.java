90603fa9838c522e4527aa5126d885816cbd0954
Adrish Bose <adrishbose198@gmail.com>
2020-11-29 12:13:54 +0530

    View added

---
+++ app/src/main/java/com/adrish/birdshooter/GameActivity.java
@@ -0,0 +1,13 @@
+package com.adrish.birdshooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/adrish/birdshooter/GameView.java
@@ -0,0 +1,10 @@
+package com.adrish.birdshooter;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView {
+    public GameView(Context context) {
+        super(context);
+    }
+}
