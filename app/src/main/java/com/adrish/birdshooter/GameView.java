b47821da328a55924867020964552555019ff86c
Adrish Bose <adrishbose198@gmail.com>
2020-11-29 12:16:53 +0530

    View added

--- app/src/main/java/com/adrish/birdshooter/GameView.java
+++ app/src/main/java/com/adrish/birdshooter/GameView.java
@@ -3,8 +3,27 @@ package com.adrish.birdshooter;
 import android.content.Context;
 import android.view.SurfaceView;
 
-public class GameView extends SurfaceView {
-    public GameView(Context context) {
+public class GameView extends SurfaceView implements Runnable
+{
+    private Thread thread;
+    public GameView(Context context)
+    {
         super(context);
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
+
+    public void resume()
+    {
+
+    }
+
+    public void pause()
+    {
+        
     }
 }
