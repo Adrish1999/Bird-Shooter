722fd162c740010f1bb773f3ee8ad2cd3e3523cc
Adrish Bose <adrishbose198@gmail.com>
2020-11-30 19:15:58 +0530

    Background decoded

--- app/src/main/java/com/adrish/birdshooter/GameView.java
+++ app/src/main/java/com/adrish/birdshooter/GameView.java
@@ -7,12 +7,16 @@ public class GameView extends SurfaceView implements Runnable
 {
     private Thread thread;
     private boolean isPlaying;
+    private int screenX, screenY;
     private Background background1, background2;
 
     public GameView(Context context, int screenX, int screenY)
     {
         super(context);
 
+        this.screenX = screenX;
+        this.screenY = screenY;
+        
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
 
@@ -34,6 +38,11 @@ public class GameView extends SurfaceView implements Runnable
     {
         background1.x -= 10;
         background2.x -= 10;
+
+        if(background1.x + background1.background.getWidth() < 0)
+        {
+            background1.x = screenX;
+        }
     }
 
     public void draw()
