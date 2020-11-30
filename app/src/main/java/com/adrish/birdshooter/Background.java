48b93eab1ae791a2241ee3393ae4044e4b8f0430
Adrish Bose <adrishbose198@gmail.com>
2020-11-30 19:05:12 +0530

    Background decoded

--- app/src/main/java/com/adrish/birdshooter/Background.java
+++ app/src/main/java/com/adrish/birdshooter/Background.java
@@ -1,4 +1,17 @@
 package com.adrish.birdshooter;
 
-public class Background {
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Background
+{
+    int x,y;
+    Bitmap background;
+
+    Background(int screenX, int screenY, Resources res)
+    {
+        background = BitmapFactory.decodeResource(res,R.drawable.background);
+        background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
+    }
 }
